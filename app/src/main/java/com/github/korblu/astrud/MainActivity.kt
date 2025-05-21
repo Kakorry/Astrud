7a22dc5935468165bfa6ddbf8595763ec69f2f48
Kakorry <jricardogames6@gmail.com>
2025-05-21 18:19:27 -0300

    Clean MainActivity

--- app/src/main/java/com/github/korblu/astrud/MainActivity.kt
+++ app/src/main/java/com/github/korblu/astrud/MainActivity.kt
@@ -1,34 +1,15 @@
 package com.github.korblu.astrud
 
 import android.os.Bundle
-import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.github.korblu.astrud.ui.theme.AstrudTheme
-
-val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.github.korblu.astrud.ui.pages.AstrudWelcome
+import com.github.korblu.astrud.ui.pages.HomePage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,76 +23,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AstrudApp() {
-    AstrudTheme {
-        Scaffold(containerColor = MaterialTheme.colorScheme.background) { innerPadding ->
-            AstrudWelcomeContent(modifier = Modifier.padding(innerPadding))
-        }
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "WelcomePage") {
+        composable("WelcomePage") { AstrudWelcome(navController) }
+        composable("HomeScreen") { HomePage() }
     }
-}
-
-@Composable
-fun AstrudWelcomeContent(modifier: Modifier = Modifier) {
-    Surface(
-        color = MaterialTheme.colorScheme.background,
-        modifier = modifier.fillMaxSize()
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(20.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                TitleText("Astrud")
-                SloganText("\"Made for all, with love for music.\"")
-                NextButton(onClick = { val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI; })
-            }
-        }
-    }
-}
-
-@Composable
-fun TitleText(text: String, modifier: Modifier = Modifier) {
-    Text(
-        text = text,
-        fontSize = 40.sp,
-        color = MaterialTheme.colorScheme.onBackground,
-        fontWeight = FontWeight.Bold,
-        modifier = modifier
-    )
-}
-
-@Composable
-fun SloganText(text: String, modifier: Modifier = Modifier) {
-    Text(
-        text = text,
-        fontSize = 20.sp,
-        fontWeight = FontWeight.Thin,
-        fontStyle = FontStyle.Italic,
-        color = MaterialTheme.colorScheme.onBackground,
-        modifier = modifier
-    )
-}
-
-@Composable
-fun NextButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
-    Button(
-        onClick = onClick,
-        contentPadding = PaddingValues(horizontal = 40.dp, vertical = 15.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
-        ),
-        modifier = modifier
-    ) {
-        Text(text = "Next ❤️", fontSize = 24.sp, textAlign = TextAlign.Center)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun AstrudAppPreview() {
-    AstrudApp()
 }
