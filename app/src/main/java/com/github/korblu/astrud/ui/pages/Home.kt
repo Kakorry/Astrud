30b0bf52551b73da03f89ca12b0dd2a2a553ddb0
Kakorry <jricardogames6@gmail.com>
2025-05-23 16:27:42 -0300

    Add Top Home Content

--- app/src/main/java/com/github/korblu/astrud/ui/pages/Home.kt
+++ app/src/main/java/com/github/korblu/astrud/ui/pages/Home.kt
@@ -4,6 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,22 +19,45 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.github.korblu.astrud.ui.theme.AstrudTheme
 
+// I can't tell if Jetpack Compose is really like this or I'm just unorganized.
+// Feels like a mess. Maybe UI Toolkits are just like this?
 @Composable
-fun HomePage(modifier: Modifier = Modifier) {
+fun AstrudHome() {
     AstrudTheme {
-        Surface(color = MaterialTheme.colorScheme.background, modifier = Modifier.fillMaxSize()) {
+        Surface(
+            color = MaterialTheme.colorScheme.background,
+            modifier = Modifier.fillMaxSize()
+        ) {
             Row(
                 verticalAlignment = Alignment.Top,
                 horizontalArrangement = Arrangement.Start
+            ) {
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .padding(start = 15.dp, top = 5.dp)
+                        .size(25.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Settings,
+                        contentDescription = "Settings",
+                        tint = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
+                }
+            }
+
+            Row(
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.Absolute.Center
             ) {
                 Text(
                     text = "As",
                     textAlign = TextAlign.Center,
                     fontWeight = FontWeight.ExtraBold,
                     fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-                    modifier = Modifier.padding(start = 15.dp, top = 50.dp),
                     color = MaterialTheme.colorScheme.onPrimaryContainer
                 )
                 Text(
@@ -36,10 +65,39 @@ fun HomePage(modifier: Modifier = Modifier) {
                     textAlign = TextAlign.Center,
                     fontWeight = FontWeight.ExtraBold,
                     fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-                    modifier = Modifier.padding(top = 50.dp),
                     color = MaterialTheme.colorScheme.primary
                 )
+            }
 
+            Row(
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.End
+            ) {
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .padding(end = 15.dp, top = 5.dp)
+                        .size(25.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+
+            Row(
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = "Suggestions:",
+                    textAlign = TextAlign.Left,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 22.sp,
+                    modifier = Modifier.padding(start = 15.dp, top = 60.dp)
+                )
             }
         }
     }
