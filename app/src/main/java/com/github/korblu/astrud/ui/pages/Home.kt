2928366f1e2f68c9d1ed9df496fb2e7281037ad4
Kakorry <jricardogames6@gmail.com>
2025-05-25 02:22:09 -0300

    Place Inside Column

--- app/src/main/java/com/github/korblu/astrud/ui/pages/Home.kt
+++ app/src/main/java/com/github/korblu/astrud/ui/pages/Home.kt
@@ -1,13 +1,21 @@
 package com.github.korblu.astrud.ui.pages
 
+import android.widget.Space
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -20,84 +28,108 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.github.korblu.astrud.ui.theme.AstrudTheme
 
 // I can't tell if Jetpack Compose is really like this or I'm just unorganized.
 // Feels like a mess. Maybe UI Toolkits are just like this?
+// It is an intuitive to build mess, though. This is pretty fun. 05/23/2025
+
 @Composable
-fun AstrudHome() {
+fun AstrudHeader() {
+    Row(
+        modifier = Modifier.padding(top = 5.dp),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .padding(start = 15.dp, top = 5.dp)
+                .size(25.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Settings,
+                contentDescription = "Settings",
+                tint = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Text(
+            text = "As",
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+            color = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+        Text(
+            text = "trud",
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+            color = MaterialTheme.colorScheme.primary
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .padding(end = 15.dp, top = 5.dp)
+                .size(25.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = "Search",
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+}
+
+@Composable
+fun AstrudHome(navController: NavController) {
     AstrudTheme {
         Surface(
             color = MaterialTheme.colorScheme.background,
             modifier = Modifier.fillMaxSize()
         ) {
-            Row(
-                verticalAlignment = Alignment.Top,
-                horizontalArrangement = Arrangement.Start
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
             ) {
-                IconButton(
-                    onClick = {},
-                    modifier = Modifier
-                        .padding(start = 15.dp, top = 5.dp)
-                        .size(25.dp)
+                AstrudHeader()
+                Row(
+                    verticalAlignment = Alignment.Top,
+                    horizontalArrangement = Arrangement.Start
                 ) {
-                    Icon(
-                        imageVector = Icons.Filled.Settings,
-                        contentDescription = "Settings",
-                        tint = MaterialTheme.colorScheme.onPrimaryContainer
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Text(
+                        text = "Speed Dial:",
+                        textAlign = TextAlign.Left,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 22.sp,
+                        modifier = Modifier.padding(top = 25.dp)
                     )
+
+                    Spacer(modifier = Modifier.weight(1f))
                 }
-            }
 
-            Row(
-                verticalAlignment = Alignment.Top,
-                horizontalArrangement = Arrangement.Absolute.Center
-            ) {
-                Text(
-                    text = "As",
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.ExtraBold,
-                    fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-                Text(
-                    text = "trud",
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.ExtraBold,
-                    fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-                    color = MaterialTheme.colorScheme.primary
-                )
-            }
-
-            Row(
-                verticalAlignment = Alignment.Top,
-                horizontalArrangement = Arrangement.End
-            ) {
-                IconButton(
+                Button(
                     onClick = {},
-                    modifier = Modifier
-                        .padding(end = 15.dp, top = 5.dp)
-                        .size(25.dp)
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Search,
-                        contentDescription = "Search",
-                        tint = MaterialTheme.colorScheme.primary
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                                        .padding(top = 15.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        contentColor = MaterialTheme.colorScheme.onPrimaryContainer
                     )
+                ) {
+                    Text(text = "Testie Button")
                 }
-            }
-
-            Row(
-                verticalAlignment = Alignment.Top,
-                horizontalArrangement = Arrangement.Start
-            ) {
-                Text(
-                    text = "Suggestions:",
-                    textAlign = TextAlign.Left,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 22.sp,
-                    modifier = Modifier.padding(start = 15.dp, top = 60.dp)
-                )
             }
         }
     }
