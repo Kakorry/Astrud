6018028cdb2241ae998150c49091074e09ab5f61
bluu <sahegope@gmail.com>
2025-07-05 03:11:30 -0300

    Update Gradle Related Scripts
    Added "KSP" and "Room" support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     kotlin("plugin.serialization") version "2.0.21"
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -65,4 +66,6 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
     androidTestImplementation(libs.androidx.navigation.testing)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
 }
