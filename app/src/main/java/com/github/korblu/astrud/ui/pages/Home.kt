67ea34d04d6b606709b64571666752ccdce5171e
Kakorry <kakorry@proton.me>
2025-07-09 18:59:30 -0300

    Update Layout

--- app/src/main/java/com/github/korblu/astrud/ui/pages/Home.kt
+++ app/src/main/java/com/github/korblu/astrud/ui/pages/Home.kt
@@ -1,13 +1,13 @@
 package com.github.korblu.astrud.ui.pages
 
 import android.app.Activity
-import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
@@ -25,15 +24,11 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -42,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -50,7 +46,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.createBitmap
 import androidx.navigation.NavController
-import com.github.korblu.astrud.AstrudAppBar
 import com.github.korblu.astrud.data.media.AudioFiles
 import com.github.korblu.astrud.ui.theme.AstrudTheme
 
@@ -61,64 +56,11 @@ import com.github.korblu.astrud.ui.theme.AstrudTheme
 */
 
 @Composable
-fun AstrudHeader() {
-    Row(
-        modifier = Modifier.padding(top = 10.dp),
-        verticalAlignment = Alignment.Top,
-        horizontalArrangement = Arrangement.Start
-    ) {
-        IconButton(
-            onClick = {},
-            modifier = Modifier
-                .padding(start = 15.dp, top = 5.dp)
-                .size(25.dp)
-        ) {
-            Icon(
-                imageVector = Icons.Filled.Settings,
-                contentDescription = "Settings",
-                tint = MaterialTheme.colorScheme.onPrimaryContainer
-            )
-        }
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Text(
-            text = "As",
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.ExtraBold,
-            fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-            color = MaterialTheme.colorScheme.onPrimaryContainer
-        )
-        Text(
-            text = "trud",
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.ExtraBold,
-            fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-            color = MaterialTheme.colorScheme.primary
-        )
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        IconButton(
-            onClick = {},
-            modifier = Modifier
-                .padding(end = 15.dp, top = 5.dp)
-                .size(25.dp)
-        ) {
-            Icon(
-                imageVector = Icons.Filled.Search,
-                contentDescription = "Search",
-                tint = MaterialTheme.colorScheme.primary
-            )
-        }
-    }
-}
-
-@Composable
 fun AstrudDial() {
     val context = LocalContext.current
     val audioFiles = remember { AudioFiles(context as Activity) }
-    val getFolder = rememberLauncherForActivityResult(audioFiles.contractDocumentTree, audioFiles.onResult)
+    val getFolder =
+        (rememberLauncherForActivityResult(audioFiles.contractDocumentTree, audioFiles.onResult))
 
     Box(
         modifier = Modifier
@@ -145,14 +87,29 @@ fun AstrudDial() {
                 items(gridItems) { item ->
                     @Composable
                     fun ImageBox(roundUnit1: Dp, roundUnit2: Dp, roundUnit3: Dp, roundUnit4: Dp) {
-                        var bitmap by remember { mutableStateOf(createBitmap(1, 1).asImageBitmap()) }
+                        var bitmap by remember {
+                            mutableStateOf(
+                                createBitmap(
+                                    1,
+                                    1
+                                ).asImageBitmap()
+                            )
+                        }
                         var click = 0
-                        Image (
+                        Image(
                             bitmap = bitmap,
                             contentDescription = "Album Cover",
                             modifier = Modifier
                                 .height(90.dp)
-                                .background(MaterialTheme.colorScheme.primary, RoundedCornerShape(topStart = roundUnit1, topEnd = roundUnit2, bottomStart = roundUnit3, bottomEnd = roundUnit4))
+                                .background(
+                                    MaterialTheme.colorScheme.primary,
+                                    RoundedCornerShape(
+                                        topStart = roundUnit1,
+                                        topEnd = roundUnit2,
+                                        bottomStart = roundUnit3,
+                                        bottomEnd = roundUnit4
+                                    )
+                                )
                                 .clickable(
                                     enabled = true,
                                     // todo Fix whatever the fuck this is -K 06/06/2025
@@ -162,8 +119,9 @@ fun AstrudDial() {
                                             getFolder.launch(null)
                                             click = 1
                                         } else {
-                                            bitmap = audioFiles.getEmbeddedPic(context)?.asImageBitmap() ?: bitmap
-                                            Log.d("test", audioFiles.getAllDefaultDirSongsMD(context).toString())
+                                            bitmap =
+                                                audioFiles.getEmbeddedPic(context)?.asImageBitmap()
+                                                    ?: bitmap
                                         }
                                     }
                                 ),
@@ -174,15 +132,19 @@ fun AstrudDial() {
                         "Item 0" -> {
                             ImageBox(12.dp, 0.dp, 0.dp, 0.dp)
                         }
+
                         "Item 2" -> {
                             ImageBox(0.dp, 12.dp, 0.dp, 0.dp)
                         }
+
                         "Item 6" -> {
                             ImageBox(0.dp, 0.dp, 12.dp, 0.dp)
                         }
+
                         "Item 8" -> {
                             ImageBox(0.dp, 0.dp, 0.dp, 12.dp)
                         }
+
                         else -> {
                             ImageBox(0.dp, 0.dp, 0.dp, 0.dp)
                         }
@@ -218,14 +180,18 @@ fun Suggestions(flavorText: String) {
             fun SongBox() {
                 Box(
                     modifier = Modifier
-                        .background(MaterialTheme.colorScheme.primary, RoundedCornerShape(18.dp))
+                        .background(
+                            MaterialTheme.colorScheme.onSecondary,
+                            RoundedCornerShape(18.dp)
+                        )
                         .width(130.dp)
                         .height(120.dp),
                     contentAlignment = Alignment.Center
                 ) {
                     Text(
                         text = song,
-                        color = MaterialTheme.colorScheme.onPrimary,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary,
                     )
                 }
             }
@@ -234,63 +200,63 @@ fun Suggestions(flavorText: String) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AstrudHome(navController: NavController) {
-    Scaffold(
-        bottomBar = {
-            AstrudAppBar(navController)
-        }
-    ) { innerPadding ->
-        AstrudTheme {
-            Surface(
-                color = MaterialTheme.colorScheme.background,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-            ) {
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Top,
+fun AstrudHome(
+    navController: NavController,
+    innerPadding: PaddingValues,
+    scrollBehavior: TopAppBarScrollBehavior
+) {
+    AstrudTheme {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .nestedScroll(scrollBehavior.nestedScrollConnection)
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+        ) {
+            item {
+                Row(
+                    modifier = Modifier.fillMaxWidth()
                 ) {
-                    item {
-                        AstrudHeader()
-                    }
+                    Spacer(modifier = Modifier.weight(1f))
 
-                    item {
-                        Row(
-                            verticalAlignment = Alignment.Top,
-                            horizontalArrangement = Arrangement.Center,
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Text(
-                                text = "Speed Dial:",
-                                textAlign = TextAlign.Left,
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 22.sp,
-                                modifier = Modifier.padding(top = 25.dp)
-                            )
-                        }
-                    }
+                    Text(
+                        text = "Speed Dial:",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 22.sp,
+                    )
 
-                    item {
-                        AstrudDial()
-                    }
-                    // todo Actually make these different and interesting. 06/04/2025 -K
-                    item {
-                        Suggestions("Suggestions:")
-                    }
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+            }
 
-                    item {
-                        Suggestions("Recently Listened:")
-                    }
+            item {
+                AstrudDial()
+            }
+            // todo Actually make these different and interesting. 06/04/2025 -K
+            item {
+                Suggestions("Suggestions:")
+            }
 
-                    item {
-                        Suggestions("Recently Added:")
-                    }
+            item {
+                Suggestions("Recently Listened:")
+            }
+
+            item {
+                Suggestions("Recently Added:")
+            }
+
+            item {
+                Button(
+                    onClick = {}
+                ) {
+
                 }
             }
         }
     }
 }
+
 
