bb86bfec1c120f290d6504e8921d531705bace88
bluu <sahegope@gmail.com>
2025-07-16 20:05:52 -0300

    Update "SongsRepo" Repository

--- app/src/main/java/com/github/korblu/astrud/data/repos/SongsRepo.kt
+++ app/src/main/java/com/github/korblu/astrud/data/repos/SongsRepo.kt
@@ -1,24 +1,40 @@
 package com.github.korblu.astrud.data.repos
 
+import android.util.Log
 import com.github.korblu.astrud.data.room.SongsDao
-import com.github.korblu.astrud.data.room.SongsEntity
+import com.github.korblu.astrud.data.room.Song
 
 // Main thing to check if you want to use database that stores all the songs.
-// todo Check if this actually works because the silly other guy (kakorry) didn't make a viewmodel yet so i can't freaking debug.
 class SongsRepo(private val songsDao: SongsDao) {
-    suspend fun addSong(song: SongsEntity): Long {
+    suspend fun addSong(song: List<Song>): List<Long> {
         return songsDao.insert(song)
     }
 
-    suspend fun updateSong(song: SongsEntity) : Int {
+    suspend fun updateSong(song: Song) : Int {
         return songsDao.update(song)
     }
 
-    suspend fun deleteSong(song: SongsEntity) : Int {
+    suspend fun deleteSong(song: Song) : Int {
         return songsDao.delete(song)
     }
 
-    suspend fun getSongById(id : Long) : SongsEntity {
+    suspend fun getSongById(id : Long) : Song? {
         return songsDao.getSongById(id)
+    }
+
+    suspend fun getAllSongs() : List<Song> {
+        return songsDao.getAllSongs()
+    }
+
+    suspend fun getSongsByAlbum(albumName: String?) : List<Song> {
+        return songsDao.getSongsByAlbum(albumName)
+    }
+
+    suspend fun getSongsByArtist(artist: String?) : List<Song> {
+        return songsDao.getSongsByArtist(artist)
+    }
+
+    suspend fun getSongsByGenre(genre: String?) : List<Song> {
+        return songsDao.getSongsByGenre(genre)
     }
 }
