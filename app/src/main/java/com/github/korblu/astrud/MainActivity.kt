a6d19c4b06a52fb6d5c02a64768132a1197f7571
Kakorry <kakorry@proton.me>
2025-07-16 21:07:48 -0300

    Add Permission Handling, TopBar States, Transitions and more

--- app/src/main/java/com/github/korblu/astrud/MainActivity.kt
+++ app/src/main/java/com/github/korblu/astrud/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.github.korblu.astrud
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.Keep
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -38,6 +48,7 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,32 +56,89 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.github.korblu.astrud.data.media.AudioPermissionHelper
 import com.github.korblu.astrud.ui.pages.AstrudHome
 import com.github.korblu.astrud.ui.pages.AstrudSongList
 import com.github.korblu.astrud.ui.theme.AstrudTheme
+import com.github.korblu.astrud.ui.viewmodels.AppBarViewModel
+import com.github.korblu.astrud.ui.viewmodels.SongViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import android.provider.Settings as AppSettings
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    lateinit var audioPermissionHelper: AudioPermissionHelper
+    val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted : Boolean ->
+        audioPermissionHelper.handleResult(isGranted)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AstrudApp()
+            val songViewModel = hiltViewModel<SongViewModel>()
+            val barViewModel= hiltViewModel<AppBarViewModel>()
+
+            val navController = rememberNavController()
+            val isLoading by songViewModel.isLoading.collectAsState()
+            val isAudioPermissionGiven by songViewModel.wasPermissionGiven.collectAsState()
+
+            audioPermissionHelper = AudioPermissionHelper(
+                context = applicationContext,
+                activity = this,
+                permissionLauncher = requestPermissionLauncher,
+                onGranted = {
+                    songViewModel.onPermissionChange(true)
+                },
+                onDenied = {
+                    val intent = Intent(AppSettings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                    songViewModel.onPermissionChange(false)
+                    Toast.makeText(applicationContext, "Please Grant Permission.", Toast.LENGTH_SHORT).show()
+                    finish()
+                },
+                onDeniedPermanently = {
+                    if (isAudioPermissionGiven == false) {
+                        val intent = Intent(AppSettings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", packageName, null)
+                        intent.data = uri
+                        startActivity(intent)
+                        songViewModel.onPermissionChange(false)
+                        Toast.makeText(
+                            applicationContext,
+                            "Please Grant Permission.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish()
+                    }
+                }
+            )
+
+            audioPermissionHelper.checkAndRequest()
+
+            if (isAudioPermissionGiven == true) {
+                 AstrudApp(songViewModel, barViewModel, navController)
+            }
         }
     }
 }
 
-// Unrelated, but did you know Astrud sung Girl from Ipanema?
+// Unrelated, but did you know Astrud sang Girl from Ipanema?
 // You should listen to it. -K 05/25/2025
-
 @OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun astrudHeader(scrollBehavior: TopAppBarScrollBehavior): TopAppBarScrollBehavior {
+@Composable // Added Pair to return two values
+fun AstrudHeader(navController: NavHostController, scrollBehavior: TopAppBarScrollBehavior, songViewModel: AppBarViewModel) {
     CenterAlignedTopAppBar(
         scrollBehavior = scrollBehavior,
         title = {
@@ -97,19 +165,18 @@ fun astrudHeader(scrollBehavior: TopAppBarScrollBehavior): TopAppBarScrollBehavi
                 Row(
                     horizontalArrangement = Arrangement.Start,
                     verticalAlignment = Alignment.Top,
+                    modifier = Modifier.padding(start = 5.dp)
                 ) {
                     Text(
                         text = "As",
                         textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.ExtraBold,
-                        fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+                        style = MaterialTheme.typography.headlineLarge,
                         color = MaterialTheme.colorScheme.onPrimaryContainer
                     )
                     Text(
                         text = "trud",
                         textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.ExtraBold,
-                        fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+                        style = MaterialTheme.typography.headlineLarge,
                         color = MaterialTheme.colorScheme.primary
                     )
                 }
@@ -138,9 +205,7 @@ fun astrudHeader(scrollBehavior: TopAppBarScrollBehavior): TopAppBarScrollBehavi
             titleContentColor = MaterialTheme.colorScheme.onSurface,
             actionIconContentColor = MaterialTheme.colorScheme.onSurface
         ),
-    )
-    return scrollBehavior
-}
+    ) }
 
 @Composable
 fun BottomBarIconButton(
@@ -160,7 +225,7 @@ fun BottomBarIconButton(
 
     IconButton(
         onClick = {
-            if (currentRoute != targetRoute) {
+            if (currentRoute != targetRoute && currentRoute != "Welcome") {
                 navController.navigate(targetRoute) {
                     val startDestinationRoute = navController.graph.findStartDestination().route
 
@@ -183,48 +248,136 @@ fun BottomBarIconButton(
     )
 }
 
+// I can't believe I had to make a separate function for this. -K 07/12/2025
+fun getCorrectTransition(currentRoute: String?, previousRoute: String?): EnterTransition? {
+    return if (currentRoute == "Home" && previousRoute == "Welcome" || previousRoute == null) {
+        fadeIn(animationSpec = tween(600))
+    } else {
+        fadeIn(animationSpec = tween(300)) + slideInHorizontally(initialOffsetX = { -it }, animationSpec = tween(300))
+    }
+}
+
+@Keep
 @Composable
 fun AstrudAppBar(navController: NavController) {
     BottomAppBar(
-        modifier = Modifier
-            .height(90.dp)
-            .fillMaxWidth(),
-        containerColor = MaterialTheme.colorScheme.surface,
-        actions = {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(55.dp)
+            modifier = Modifier
+                .fillMaxWidth(),
+            containerColor = MaterialTheme.colorScheme.surface,
+            actions = {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
                 ) {
-                    BottomBarIconButton(navController, "Home", Icons.Filled.Home, "Home")
-                    BottomBarIconButton(navController, "List", Icons.Filled.LibraryMusic, "Songs")
-                    BottomBarIconButton(navController, icon = Icons.Filled.Album, description =  "Albums")
-                    BottomBarIconButton(navController, icon = Icons.Filled.Person, description = "Artists")
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(55.dp)
+                    ) {
+                        BottomBarIconButton(navController, "Home", Icons.Filled.Home, "Home")
+                        BottomBarIconButton(
+                            navController,
+                            "List",
+                            Icons.Filled.LibraryMusic,
+                            "Songs"
+                        )
+                        BottomBarIconButton(
+                            navController,
+                            icon = Icons.Filled.Album,
+                            description = "Albums"
+                        )
+                        BottomBarIconButton(
+                            navController,
+                            icon = Icons.Filled.Person,
+                            description = "Artists"
+                        )
+                    }
                 }
             }
-        }
-    )
+        )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AstrudApp() {
+fun AstrudApp(
+    songViewModel: SongViewModel = hiltViewModel<SongViewModel>(),
+    barViewModel: AppBarViewModel = hiltViewModel<AppBarViewModel>(),
+    navController: NavHostController
+) {
     AstrudTheme {
-        val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
-        val navController = rememberNavController()
-
+        val astrudUri by songViewModel.astrudMusicUri.collectAsState()
         val navBackStackEntry by navController.currentBackStackEntryAsState()
-        LaunchedEffect(navBackStackEntry?.destination) {
-            scrollBehavior.state.heightOffset = 0f
-            scrollBehavior.state.contentOffset = 0f
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        val homeAppBarState = rememberTopAppBarState()
+        val listAppBarState = rememberTopAppBarState()
+
+        val astrudHeaderState = when (currentRoute) {
+            "Home" -> TopAppBarDefaults.pinnedScrollBehavior(homeAppBarState)
+            "List" -> TopAppBarDefaults.pinnedScrollBehavior(listAppBarState)
+            else -> TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
         }
+        val previousRoute = navController.previousBackStackEntry?.destination?.route
+        val playedAnimationStatus by barViewModel.playedBarAnimation.collectAsState()
 
         Scaffold(
-            bottomBar = { AstrudAppBar(navController) },
-            topBar = { astrudHeader(scrollBehavior) }
+            bottomBar = {
+                AnimatedVisibility(
+                    visible = true,
+                    enter = if (!playedAnimationStatus) {
+                        slideInVertically(
+                            initialOffsetY = { it },
+                            animationSpec = tween(
+                                durationMillis = 600,
+                                easing = LinearOutSlowInEasing
+                            )
+                        )
+                    } else {
+                        EnterTransition.None
+                    },
+                    exit = if (!playedAnimationStatus) {
+                        slideOutVertically(
+                            targetOffsetY = { -it },
+                            animationSpec = tween(
+                                durationMillis = 600,
+                                easing = LinearOutSlowInEasing
+                            )
+                        )
+                    } else {
+                        ExitTransition.None
+                    },
+                ) {
+                    AstrudAppBar(navController)
+                }
+            },
+            topBar = {
+                AnimatedVisibility(
+                    visible = true,
+                    enter = if (!playedAnimationStatus) {
+                        slideInVertically(
+                            initialOffsetY = { -it },
+                            animationSpec = tween(
+                                durationMillis = 600,
+                                easing = LinearOutSlowInEasing
+                            )
+                        )
+                    } else {
+                        EnterTransition.None
+                    },
+                    exit = if (!playedAnimationStatus) {
+                        slideOutVertically(
+                            targetOffsetY = { it },
+                            animationSpec = tween(
+                                durationMillis = 600,
+                                easing = LinearOutSlowInEasing
+                            )
+                        )
+                    } else {
+                        ExitTransition.None
+                    },
+                ) {
+                    AstrudHeader(navController = navController, scrollBehavior = astrudHeaderState, barViewModel)
+                }
+            }
         ) { innerPadding ->
             NavHost(
                 navController = navController,
@@ -232,18 +385,32 @@ fun AstrudApp() {
             ) {
                 composable(
                     "Home",
-                    enterTransition = { fadeIn(animationSpec = tween(300)) + slideInHorizontally(initialOffsetX = { -it }, animationSpec = tween(300)) },
+                    enterTransition = {
+                        getCorrectTransition(currentRoute, previousRoute)
+                    },
                     exitTransition = { fadeOut(animationSpec = tween(300)) + slideOutHorizontally(targetOffsetX = { -it }, animationSpec = tween(300)) },
                     popEnterTransition = { fadeIn(animationSpec = tween(300)) + slideInHorizontally(initialOffsetX = { -it }, animationSpec = tween(300)) },
                     popExitTransition = { fadeOut(animationSpec = tween(300)) + slideOutHorizontally(targetOffsetX = { -it }, animationSpec = tween(300)) }
-                ) { AstrudHome(navController, innerPadding, scrollBehavior) }
+                ) {
+                    AstrudHome(
+                        navController,
+                        innerPadding,
+                        scrollBehavior = astrudHeaderState,
+                        songViewModel = songViewModel,
+                        barViewModel = barViewModel
+                    )
+
+                    LaunchedEffect(true) {
+                        barViewModel.onSetPlayedStatus()
+                    }
+                }
                 composable(
                     "List",
                     enterTransition = { fadeIn(animationSpec = tween(300)) + slideInHorizontally(initialOffsetX = { it }, animationSpec = tween(300)) },
                     exitTransition = { fadeOut(animationSpec = tween(300)) + slideOutHorizontally(targetOffsetX = { it }, animationSpec = tween(300)) },
                     popEnterTransition = { fadeIn(animationSpec = tween(300)) + slideInHorizontally(initialOffsetX = { it }, animationSpec = tween(300)) },
                     popExitTransition = { fadeOut(animationSpec = tween(300)) + slideOutHorizontally(targetOffsetX = { it }, animationSpec = tween(300)) }
-                ) { AstrudSongList(innerPadding, scrollBehavior) }
+                ) { AstrudSongList(innerPadding, astrudHeaderState, barViewModel) }
             }
         }
     }
