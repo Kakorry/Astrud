bf15d954ab4d604560af583195b586cc80d140cd
Kakorry <kakorry@proton.me>
2025-07-16 21:02:59 -0300

    Add Songs to Home Page

--- app/src/main/java/com/github/korblu/astrud/ui/pages/Home.kt
+++ app/src/main/java/com/github/korblu/astrud/ui/pages/Home.kt
@@ -1,8 +1,7 @@
 package com.github.korblu.astrud.ui.pages
 
 import android.app.Activity
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.compose.foundation.Image
+import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -10,44 +9,55 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.graphics.createBitmap
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.github.korblu.astrud.data.media.AudioFiles
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.error
+import coil3.request.fallback
+import coil3.request.placeholder
+import coil3.request.transitionFactory
+import coil3.transition.CrossfadeTransition
+import com.github.korblu.astrud.R
+import com.github.korblu.astrud.data.media.ExoPlayerManager
+import com.github.korblu.astrud.data.media.UserSongs
 import com.github.korblu.astrud.ui.theme.AstrudTheme
+import com.github.korblu.astrud.ui.viewmodels.AppBarViewModel
+import com.github.korblu.astrud.ui.viewmodels.SongViewModel
 
 /*
     I can't tell if Jetpack Compose is really like this or I'm just unorganized.
@@ -56,99 +66,118 @@ import com.github.korblu.astrud.ui.theme.AstrudTheme
 */
 
 @Composable
-fun AstrudDial() {
+fun SongArtwork(
+    modifier: Modifier = Modifier,
+    scale: ContentScale = ContentScale.Crop,
+    songAlbumUri: String? = null,
+    songTitle: String? = null,
+    songUri: String? = null,
+    rounding: Dp = 12.dp
+) {
     val context = LocalContext.current
-    val audioFiles = remember { AudioFiles(context as Activity) }
-    val getFolder =
-        (rememberLauncherForActivityResult(audioFiles.contractDocumentTree, audioFiles.onResult))
+
+    Box(
+        modifier = modifier
+            .clickable(
+                onClick = {
+                    val exoPlayer = ExoPlayerManager(context)
+                    exoPlayer.initPlayer()
+                    if (songUri != null)
+                        exoPlayer.playAudio(songUri)
+                }
+            ),
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(songAlbumUri)
+                .placeholder(R.drawable.album_placeholder)
+                .fallback(R.drawable.album_placeholder)
+                .error(R.drawable.album_placeholder)
+                .transitionFactory(CrossfadeTransition.Factory())
+                .build(),
+            contentDescription = "Album Cover",
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 1.dp)
+                .clip(RoundedCornerShape(size = rounding)),
+            contentScale = scale,
+        )
+
+        if (songTitle != null) {
+            Box(
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.onSecondary)
+                    .fillMaxWidth()
+                    .heightIn(min = 30.dp)
+                    .align(Alignment.BottomCenter),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = songTitle,
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.labelSmall,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(horizontal = 10.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun getRandomSongs(songViewModel: SongViewModel, listSize: Int = 10): List<Map<String?, String?>?>? {
+    val activity = LocalActivity.current as Activity
+    val userSongs = UserSongs(songViewModel)
+    var songAttributes by remember { mutableStateOf<List<Map<String?, String?>?>?>( List (listSize) { mapOf ( null to null ) } ) }
+
+    LaunchedEffect(true) {
+        songAttributes = List (listSize) { userSongs.getRandomSong(activity, true) }
+    }
+
+    return songAttributes
+}
+
+@Composable
+fun AstrudDial(songViewModel: SongViewModel) {
+    val songAttributes = getRandomSongs(songViewModel, 9)
 
     Box(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .padding(horizontal = 40.dp),
         contentAlignment = Alignment.Center
     ) {
         Box(
             modifier = Modifier
                 .padding(top = 20.dp)
-                .heightIn(max = 280.dp)
-                .widthIn(max = 310.dp)
+                .heightIn(max = 380.dp)
+                .aspectRatio(1f)
         ) {
-            val gridItems = List(9) { "Item $it" }
-
             LazyVerticalGrid(
                 columns = GridCells.Fixed(3),
-                modifier = Modifier
-                    .height(286.dp)
-                    .padding(horizontal = 8.dp),
-                horizontalArrangement = Arrangement.spacedBy(2.dp),
-                verticalArrangement = Arrangement.spacedBy(3.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
                 userScrollEnabled = false
             ) {
-                items(gridItems) { item ->
-                    @Composable
-                    fun ImageBox(roundUnit1: Dp, roundUnit2: Dp, roundUnit3: Dp, roundUnit4: Dp) {
-                        var bitmap by remember {
-                            mutableStateOf(
-                                createBitmap(
-                                    1,
-                                    1
-                                ).asImageBitmap()
-                            )
-                        }
-                        var click = 0
-                        Image(
-                            bitmap = bitmap,
-                            contentDescription = "Album Cover",
-                            modifier = Modifier
-                                .height(90.dp)
-                                .background(
-                                    MaterialTheme.colorScheme.primary,
-                                    RoundedCornerShape(
-                                        topStart = roundUnit1,
-                                        topEnd = roundUnit2,
-                                        bottomStart = roundUnit3,
-                                        bottomEnd = roundUnit4
-                                    )
+                items(9) { index ->
+                    val songAlbumUri = songAttributes?.get(index)?.get("albumArtUri")
+                    val songTitle = songAttributes?.get(index)?.get("title")
+                    val songUri = songAttributes?.get(index)?.get("uri")
+
+                    SongArtwork(
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .clip(
+                                RoundedCornerShape(
+                                    size = 12.dp
                                 )
-                                .clickable(
-                                    enabled = true,
-                                    // todo Fix whatever the fuck this is -K 06/06/2025
-                                    // So true big K what the fuck is this -B 06/06/2025
-                                    onClick = {
-                                        if (click == 0) {
-                                            getFolder.launch(null)
-                                            click = 1
-                                        } else {
-                                            bitmap =
-                                                audioFiles.getEmbeddedPic(context)?.asImageBitmap()
-                                                    ?: bitmap
-                                        }
-                                    }
-                                ),
-                        )
-                    }
-
-                    when (item) {
-                        "Item 0" -> {
-                            ImageBox(12.dp, 0.dp, 0.dp, 0.dp)
-                        }
-
-                        "Item 2" -> {
-                            ImageBox(0.dp, 12.dp, 0.dp, 0.dp)
-                        }
-
-                        "Item 6" -> {
-                            ImageBox(0.dp, 0.dp, 12.dp, 0.dp)
-                        }
-
-                        "Item 8" -> {
-                            ImageBox(0.dp, 0.dp, 0.dp, 12.dp)
-                        }
-
-                        else -> {
-                            ImageBox(0.dp, 0.dp, 0.dp, 0.dp)
-                        }
-                    }
+                            ),
+                        songAlbumUri = songAlbumUri,
+                        songTitle = songTitle,
+                        rounding = 12.dp,
+                        songUri = songUri
+                    )
                 }
             }
         }
@@ -156,16 +185,18 @@ fun AstrudDial() {
 }
 
 @Composable
-fun Suggestions(flavorText: String) {
+fun SongSuggestions(flavorText: String, songViewModel: SongViewModel) {
+    val songAttributes = getRandomSongs(songViewModel, 10)
+
     Row(
         horizontalArrangement = Arrangement.Start,
         modifier = Modifier.fillMaxWidth(),
     ) {
         Text(
             text = flavorText,
-            textAlign = TextAlign.Left,
+            style = MaterialTheme.typography.headlineSmall,
             fontWeight = FontWeight.Bold,
-            fontSize = 22.sp,
+            textAlign = TextAlign.Start,
             modifier = Modifier.padding(top = 25.dp, start = 15.dp)
         )
     }
@@ -173,25 +204,32 @@ fun Suggestions(flavorText: String) {
         modifier = Modifier.padding(start = 15.dp, top = 20.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        val suggestionList = List(10) { "Suggestion $it" }
+        items(songAttributes?.size ?: 0) { song ->
+            val songAlbumUri = songAttributes?.get(song)?.get("albumArtUri")
+            val songTitle = songAttributes?.get(song)?.get("title")
+            val songUri = songAttributes?.get(song)?.get("uri")
 
-        items(suggestionList) { song ->
             @Composable
             fun SongBox() {
                 Box(
                     modifier = Modifier
-                        .background(
-                            MaterialTheme.colorScheme.onSecondary,
-                            RoundedCornerShape(18.dp)
-                        )
-                        .width(130.dp)
-                        .height(120.dp),
+                        .width(150.dp)
+                        .height(150.dp),
                     contentAlignment = Alignment.Center
                 ) {
-                    Text(
-                        text = song,
-                        fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colorScheme.primary,
+                    SongArtwork(
+                        scale = ContentScale.Crop,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .clip(
+                                RoundedCornerShape(
+                                    size = 18.dp
+                                )
+                            ),
+                        songAlbumUri = songAlbumUri,
+                        songTitle = songTitle,
+                        songUri = songUri,
+                        rounding = 18.dp
                     )
                 }
             }
@@ -205,6 +243,8 @@ fun Suggestions(flavorText: String) {
 fun AstrudHome(
     navController: NavController,
     innerPadding: PaddingValues,
+    songViewModel: SongViewModel = hiltViewModel<SongViewModel>(),
+    barViewModel: AppBarViewModel = hiltViewModel<AppBarViewModel>(),
     scrollBehavior: TopAppBarScrollBehavior
 ) {
     AstrudTheme {
@@ -215,6 +255,7 @@ fun AstrudHome(
                 .padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top,
+            state = barViewModel.homeBarState.collectAsState().value
         ) {
             item {
                 Row(
@@ -224,8 +265,9 @@ fun AstrudHome(
 
                     Text(
                         text = "Speed Dial:",
+                        style = MaterialTheme.typography.headlineSmall,
                         fontWeight = FontWeight.Bold,
-                        fontSize = 22.sp,
+                        modifier = Modifier.padding(start = 5.dp),
                     )
 
                     Spacer(modifier = Modifier.weight(1f))
@@ -233,27 +275,19 @@ fun AstrudHome(
             }
 
             item {
-                AstrudDial()
+                AstrudDial(songViewModel)
             }
-            // todo Actually make these different and interesting. 06/04/2025 -K
+            // todo Actually make these different and interesting. 07/04/2025 -K
             item {
-                Suggestions("Suggestions:")
-            }
-
-            item {
-                Suggestions("Recently Listened:")
+                SongSuggestions("Suggestions:", songViewModel)
             }
 
             item {
-                Suggestions("Recently Added:")
+                SongSuggestions("Recently Listened:", songViewModel)
             }
 
             item {
-                Button(
-                    onClick = {}
-                ) {
-
-                }
+                SongSuggestions("Recently Added:", songViewModel)
             }
         }
     }
