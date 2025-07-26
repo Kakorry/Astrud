423fc89250032c584bff8a77d0331f5558f39ff5
Kakorry <kakorry@proton.me>
2025-07-26 13:55:46 -0300

    Add Provider for DataStore

--- app/src/main/java/com/github/korblu/astrud/data/room/DatabaseModule.kt
+++ app/src/main/java/com/github/korblu/astrud/data/room/DatabaseModule.kt
@@ -1,12 +1,20 @@
 package com.github.korblu.astrud.data.room
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.dataStoreFile
 import androidx.room.Room
+import com.github.korblu.astrud.data.datastore.LayoutSongsList
+import com.github.korblu.astrud.data.datastore.LayoutSongsListSerializer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 @Module
@@ -20,6 +28,19 @@ object DatabaseModule {
             AppDatabase::class.java,
             "astrud_app_db"
         ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideLayoutSongsListDataStore(@ApplicationContext appContext: Context): DataStore<LayoutSongsList> {
+        return androidx.datastore.core.DataStoreFactory.create(
+            serializer = LayoutSongsListSerializer,
+            produceFile = { appContext.dataStoreFile("layout_songs_list.pb") },
+            corruptionHandler = ReplaceFileCorruptionHandler {
+                LayoutSongsList.getDefaultInstance()
+            },
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+        )
     }
 
     @Provides
