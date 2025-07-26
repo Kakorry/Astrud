b302e8c70670cd5408eb3d14069e3d20e59a5241
bluu <sahegope@gmail.com>
2025-07-26 01:24:46 -0300

    Rename Room Database's "Song" to "RoomSong"

--- app/src/main/java/com/github/korblu/astrud/data/room/SongsDao.kt
+++ app/src/main/java/com/github/korblu/astrud/data/room/SongsDao.kt
@@ -9,26 +9,26 @@ import androidx.room.Update
 @Dao
 interface SongsDao {
     @Insert
-    suspend fun insert(song: List<Song>) : List<Long>
+    suspend fun insert(roomSong: List<RoomSong>) : List<Long>
 
     @Update
-    suspend fun update(song: Song) : Int
+    suspend fun update(roomSong: RoomSong) : Int
 
     @Delete
-    suspend fun delete(song: Song) : Int
+    suspend fun delete(roomSong: RoomSong) : Int
 
     @Query("SELECT * FROM songs WHERE id = :songId")
-    suspend fun getSongById(songId: Long) : Song?
+    suspend fun getSongById(songId: Long) : RoomSong?
 
     @Query("SELECT * FROM songs")
-    suspend fun getAllSongs() : List<Song>
+    suspend fun getAllSongs() : List<RoomSong>
 
     @Query("SELECT * FROM songs WHERE album = :albumName")
-    suspend fun getSongsByAlbum(albumName: String?) : List<Song>
+    suspend fun getSongsByAlbum(albumName: String?) : List<RoomSong>
 
     @Query("SELECT * FROM songs WHERE artist = :artist")
-    suspend fun getSongsByArtist(artist: String?) : List<Song>
+    suspend fun getSongsByArtist(artist: String?) : List<RoomSong>
 
     @Query("SELECT * FROM songs WHERE genre = :genre")
-    suspend fun getSongsByGenre(genre: String?) : List<Song>
+    suspend fun getSongsByGenre(genre: String?) : List<RoomSong>
 }
