c3f5eca98f67e9dc0eb765aa9c21d7cf9fca1319
bluu <sahegope@gmail.com>
2025-07-28 00:23:28 -0300

    Remove Old Repository And Add New Repository

--- app/src/main/java/com/github/korblu/astrud/data/repos/RepositoryModule.kt
+++ app/src/main/java/com/github/korblu/astrud/data/repos/RepositoryModule.kt
@@ -1,18 +1,18 @@
 package com.github.korblu.astrud.data.repos
 
+import com.github.korblu.astrud.data.room.dao.RoomRecentsDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import com.github.korblu.astrud.data.room.SongsDao
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object SongRepositoryModule {
+object RepositoryModule {
     @Provides
     @Singleton
-    fun provideSongRepository(songDao: SongsDao): SongsRepo {
-        return SongsRepo(songDao)
+    fun provideRoomRecentsRepository(recentsDao: RoomRecentsDao) : RoomRecentsRepo {
+        return RoomRecentsRepo(recentsDao)
     }
 }
