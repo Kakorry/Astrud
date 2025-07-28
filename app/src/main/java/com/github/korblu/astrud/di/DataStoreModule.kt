d641fe9df0480a56a609a5171dec1b69737176d8
bluu <sahegope@gmail.com>
2025-07-28 00:27:20 -0300

    Add LayoutSongsList Injection

--- app/src/main/java/com/github/korblu/astrud/di/DataStoreModule.kt
+++ app/src/main/java/com/github/korblu/astrud/di/DataStoreModule.kt
@@ -3,6 +3,8 @@ package com.github.korblu.astrud.di
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.dataStore
+import com.github.korblu.astrud.data.datastore.LayoutSongsList
+import com.github.korblu.astrud.data.datastore.LayoutSongsListSerializer
 import com.github.korblu.astrud.data.datastore.UserPreferences
 import com.github.korblu.astrud.data.datastore.UserPreferencesSerializer
 import dagger.Module
@@ -17,15 +19,25 @@ private val Context.userPreferencesDataStore: DataStore<UserPreferences> by data
     serializer = UserPreferencesSerializer
 )
 
+private val Context.layoutSongsListDataStore: DataStore<LayoutSongsList> by dataStore(
+    fileName = "layout_songs_list.pb",
+    serializer = LayoutSongsListSerializer
+)
+
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
-
     @Provides
     @Singleton
     fun providesUserPreferencesDataStore(
         @ApplicationContext context: Context
     ) : DataStore<UserPreferences> {
         return context.userPreferencesDataStore
+    }
+
+    fun providesLayoutSongsListDataStore(
+        @ApplicationContext context: Context
+    ) : DataStore<LayoutSongsList> {
+        return context.layoutSongsListDataStore
     }
 }
