2d3f00c34f23429104060bf3c3af14b717daf1a9
bluu <sahegope@gmail.com>
2025-07-28 00:25:56 -0300

    Move Class And Update

--- app/src/main/java/com/github/korblu/astrud/di/DatabaseModule.kt
+++ app/src/main/java/com/github/korblu/astrud/di/DatabaseModule.kt
@@ -1,12 +1,15 @@
-package com.github.korblu.astrud.data.room
+package com.github.korblu.astrud.di
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
 import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.dataStoreFile
 import androidx.room.Room
 import com.github.korblu.astrud.data.datastore.LayoutSongsList
 import com.github.korblu.astrud.data.datastore.LayoutSongsListSerializer
+import com.github.korblu.astrud.data.room.AppDatabase
+import com.github.korblu.astrud.data.room.dao.RoomRecentsDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -33,7 +36,7 @@ object DatabaseModule {
     @Singleton
     @Provides
     fun provideLayoutSongsListDataStore(@ApplicationContext appContext: Context): DataStore<LayoutSongsList> {
-        return androidx.datastore.core.DataStoreFactory.create(
+        return DataStoreFactory.create(
             serializer = LayoutSongsListSerializer,
             produceFile = { appContext.dataStoreFile("layout_songs_list.pb") },
             corruptionHandler = ReplaceFileCorruptionHandler {
@@ -43,9 +46,9 @@ object DatabaseModule {
         )
     }
 
-    @Provides
     @Singleton
-    fun provideSongsDao(appDatabase: AppDatabase): SongsDao {
-        return appDatabase.songDao()
+    @Provides
+    fun provideRoomRecentsDao(appDatabase: AppDatabase): RoomRecentsDao {
+        return appDatabase.roomRecentsDao()
     }
 }
