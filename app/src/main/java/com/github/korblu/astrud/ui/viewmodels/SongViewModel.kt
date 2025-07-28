5c1cd2b6f5312963a20b908e0099d8ee1c7c279e
bluu <sahegope@gmail.com>
2025-07-28 00:26:24 -0300

    Remove Old Room Table Related Fields

--- app/src/main/java/com/github/korblu/astrud/ui/viewmodels/SongViewModel.kt
+++ app/src/main/java/com/github/korblu/astrud/ui/viewmodels/SongViewModel.kt
@@ -6,9 +6,7 @@ import com.github.korblu.astrud.data.datastore.LayoutSong
 import com.github.korblu.astrud.data.datastore.LayoutSongs
 import com.github.korblu.astrud.data.datastore.UserPreferences
 import com.github.korblu.astrud.data.repos.LayoutSongsRepo
-import com.github.korblu.astrud.data.repos.SongsRepo
 import com.github.korblu.astrud.data.repos.UserPreferencesRepo
-import com.github.korblu.astrud.data.room.RoomSong
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -20,14 +18,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SongViewModel @Inject constructor(
-    private val songsRepo: SongsRepo,
     private val userPrefRepo: UserPreferencesRepo,
     private val layoutSongsRepo: LayoutSongsRepo
 ) : ViewModel() {
-
-    private val _randomRoomSong = MutableStateFlow<RoomSong?>(null)
-    val randomRoomSong = _randomRoomSong.asStateFlow()
-
     private val _isLoading = MutableStateFlow(true)
     val isLoading = _isLoading.asStateFlow()
 
@@ -36,24 +29,6 @@ class SongViewModel @Inject constructor(
 
     private val _layoutSongObjectList = MutableStateFlow<List<LayoutSong>>(listOf())
     val layoutSongObjectList = _layoutSongObjectList.asStateFlow()
-
-    fun onUpdateRoomSong(roomSong: RoomSong) {
-        viewModelScope.launch {
-            songsRepo.updateSong(roomSong)
-        }
-    }
-
-    fun onDeleteRoomSong(roomSong: RoomSong) {
-        viewModelScope.launch {
-            songsRepo.deleteSong(roomSong)
-        }
-    }
-
-    fun onGetRoomSongById(id: Long) {
-        viewModelScope.launch {
-            songsRepo.getSongById(id)
-        }
-    }
 
     // For tests. Will probably be removed.
     val userPreferences: StateFlow<UserPreferences> = userPrefRepo.userPreferencesFlow
