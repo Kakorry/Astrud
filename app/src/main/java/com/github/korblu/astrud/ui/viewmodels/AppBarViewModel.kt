2d5362c41d3548faaa2e4fd175c404ad8ce583c3
Kakorry <kakorry@proton.me>
2025-08-03 22:01:02 -0300

    Add MediaItemVisibility to manage NowPlayingExtension Visibility.

--- app/src/main/java/com/github/korblu/astrud/ui/viewmodels/AppBarViewModel.kt
+++ app/src/main/java/com/github/korblu/astrud/ui/viewmodels/AppBarViewModel.kt
@@ -3,6 +3,7 @@ package com.github.korblu.astrud.ui.viewmodels
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -18,15 +19,18 @@ class AppBarViewModel @Inject constructor() : ViewModel() {
     private val _listBarState = MutableStateFlow(LazyListState())
     val listBarState = _listBarState.asStateFlow()
 
+    private val _mediaItemVisible = MutableStateFlow(false)
+    val mediaItemVisible = _mediaItemVisible.asStateFlow()
+
     private val _playedBarAnimation = MutableStateFlow(false)
     val playedBarAnimation = _playedBarAnimation.asStateFlow()
+
+    private val _nowPlayingClickState = MutableStateFlow(false)
+    val nowPlayingClickState = _nowPlayingClickState.asStateFlow()
 
     init {
         _playedBarAnimation.value = false
     }
-
-    private val _homeBarState = MutableStateFlow(LazyListState())
-    val homeBarState = _homeBarState.asStateFlow()
 
     fun onResetPlayedStatus() {
         _playedBarAnimation.value = false
@@ -46,5 +50,31 @@ class AppBarViewModel @Inject constructor() : ViewModel() {
 
     fun onSetPlayedStatus() {
         _playedBarAnimation.value = true
+    }
+
+    fun onClickedNowPlaying() {
+        _nowPlayingClickState.value = true
+    }
+
+    fun onResetNowPlaying() {
+        _nowPlayingClickState.value = false
+    }
+
+    fun onSetMediaItemVisibility() {
+        _mediaItemVisible.value = true
+    }
+
+    fun onResetMediaItemVisibility() {
+        _mediaItemVisible.value = false
+    }
+
+    fun onNavigateToNowPlaying(
+        navController: NavController,
+        uri: String?,
+        title: String?,
+        artist: String?,
+        artwork: String?
+    ) {
+        navController.navigate("NowPlayingScreen/$uri/$title/$artist/$artwork")
     }
 }
