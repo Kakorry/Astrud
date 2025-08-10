fc47ad3f7676de211ab9529ab5fd6ebc409c0e31
Kakorry <kakorry@proton.me>
2025-08-10 01:30:08 -0300

    Add Class to Convert Time and SongMetadata Class

--- app/src/main/java/com/github/korblu/astrud/AstrudApplication.kt
+++ app/src/main/java/com/github/korblu/astrud/AstrudApplication.kt
@@ -4,17 +4,48 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.net.Uri
 import android.provider.MediaStore
 import androidx.compose.ui.unit.dp
+import androidx.core.app.NotificationCompat
 import dagger.hilt.android.HiltAndroidApp
+import java.util.Locale
+import kotlin.math.roundToInt
 
 object AppConstants {
+    data class SongMetadata(
+        val uri: Uri?,
+        val title: String?,
+        val artist: String?,
+        val album: String?,
+        val albumArtUri: Uri?
+    )
+
+    class TimeConverter {
+        fun convertProgress(currentProgress: Float, songDuration: Float): String {
+            val currentSeconds = (currentProgress * songDuration / 1000).roundToInt()
+            val minutes = currentSeconds / 60
+            val seconds = currentSeconds % 60
+
+            return String.format(Locale.US, "%01d:%02d", minutes, seconds)
+        }
+
+        fun convertFullDuration(songDuration: Int): String {
+            val currentSeconds = songDuration / 1000
+            val minutes = currentSeconds / 60
+            val seconds = currentSeconds % 60
+
+            return String.format(Locale.US, "%01d:%02d", minutes, seconds)
+        }
+    }
     val BASE_PROJECTION = arrayOf(
         MediaStore.Audio.Media.TITLE,
         MediaStore.Audio.Media.ARTIST,
+        MediaStore.Audio.Media.ALBUM,
         MediaStore.Audio.Media._ID,
         MediaStore.Audio.Media.ALBUM_ID,
-        MediaStore.Audio.Media.ALBUM,
+        MediaStore.Audio.Media.DURATION,
+        MediaStore.Audio.Media.YEAR
     )
     const val MEDIA_NOTIFICATION_CHANNEL_ID = "media_playback_channel"
     const val MEDIA_NOTIFICATION_ID = 1965
@@ -28,19 +59,4 @@ object AppConstants {
 }
 
 @HiltAndroidApp
-class AstrudApplication: Application() {
-    override fun onCreate() {
-        super.onCreate()
-        createMediaNotificationChannel()
-    }
-
-    private fun createMediaNotificationChannel() {
-        val channel = NotificationChannel(
-            AppConstants.MEDIA_NOTIFICATION_CHANNEL_ID,
-            "Media Playback",
-            NotificationManager.IMPORTANCE_LOW
-        )
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-    }
-}
+class AstrudApplication: Application()
